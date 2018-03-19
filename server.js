a707f398cc3ff19c23eb225f3d100a7184fc4def
Vince Aggrippino <yhqktpi@gmail.com>
2018-03-19 11:08:50 +0800

    Correct IP address determination on servers with a proxy (glitch.me)

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.set('trust proxy', true)
 app.get('/api/whoami', (req, res) => {
-  let ipaddress = req.connection.remoteAddr || '127.0.0.1'
+  let ipaddress = req.ip
   let language = req.headers['accept-language'].split(',')[0]
   let software = req.headers['user-agent'].match(/\((.*?)\)/)[1]
 
