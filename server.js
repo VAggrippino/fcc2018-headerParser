976856ea3265382371ad743a51e36697dc009572
Vince Aggrippino <yhqktpi@gmail.com>
2018-03-19 11:28:41 +0800

    Add an HTML view for the '/' request

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.set('trust proxy', true)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
 app.get('/api/whoami', (req, res) => {
   let ipaddress = req.ip
   let language = req.headers['accept-language'].split(',')[0]
